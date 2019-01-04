1681d423e67f6743fb29e717afdc80f33a616164
Stephen Davies <steve9164@gmail.com>
2019-01-04 16:32:09 +1100

    Improve NCSS logo example

--- quokka_images/examples/ncss_logo.py
+++ quokka_images/examples/ncss_logo.py
@@ -1,4 +1,16 @@
 import quokka
 from load_image import load_image
 
-fb = load_image('NCSS-logo-54x40.qimz')
+fb = load_image('NCSS-logo-68x50.qimz')
+
+quokka.display.fill(1)
+quokka.display.blit(fb, 30, 7)
+quokka.display.show()
+
+c = 1
+
+while True:
+  quokka.sleep(1500)
+  quokka.display.invert(c)
+  quokka.display.show()
+  c = (c + 1) % 2
