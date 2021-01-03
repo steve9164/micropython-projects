abca3867e9a0b253ac96969636a33fcc10a2f304
Stephen Davies <steve9164@gmail.com>
2021-01-03 17:13:23 +1100

    update send demo

--- microbit/radio-demo-send.py
+++ microbit/radio-demo-send.py
@@ -1,18 +1,24 @@
-from microbit import *
-
+import quokka
 import radio
+import sys
 
-radio.config(channel=40)
+v = radio.version()
+if not v:
+  quokka.display.print('bad radio')
+  quokka.display.show()
+  sys.exit(0)
 
 radio.on()
+radio.config(channel=40)
 
 while True:
-  msg = radio.receive()
-  if msg is not None:
-    if msg == "duck":
-      display.show(Image.DUCK)
-    elif msg == "giraffe":
-      display.show(Image.GIRAFFE)
-    elif msg == "clear":
-      display.clear()
-
+  if quokka.buttons.a.is_pressed():
+    quokka.neopixels.set_pixel(0, 64, 0, 0)
+    radio.send("duck")
+  elif quokka.buttons.b.is_pressed():
+    radio.send("giraffe")
+    quokka.neopixels.set_pixel(0, 0, 64, 0)
+  else:
+    radio.send("clear")
+    quokka.neopixels.set_pixel(0, 0, 0, 64)
+  sleep(10)
