48861f8ffd18e4a8d8075dff808565fc52e4d2a8
Stephen Davies <steve9164@gmail.com>
2021-01-03 18:00:20 +1100

    Working Quokka -> microbit radio demo

--- microbit/radio-demo-send.py
+++ microbit/radio-demo-send.py
@@ -12,13 +12,14 @@ radio.on()
 radio.config(channel=40)
 
 while True:
-  if quokka.buttons.a.is_pressed():
-    quokka.neopixels.set_pixel(0, 64, 0, 0)
+  if quokka.button_a.is_pressed():
+    quokka.display.print('send a')
+    #quokka.neopixels.set_pixel(0, 64, 0, 0)
     radio.send("duck")
-  elif quokka.buttons.b.is_pressed():
+  elif quokka.button_b.is_pressed():
     radio.send("giraffe")
-    quokka.neopixels.set_pixel(0, 0, 64, 0)
+    #quokka.neopixels.set_pixel(0, 0, 64, 0)
   else:
     radio.send("clear")
-    quokka.neopixels.set_pixel(0, 0, 0, 64)
-  sleep(10)
+    #quokka.neopixels.set_pixel(0, 0, 0, 64)
+  quokka.sleep(10)
